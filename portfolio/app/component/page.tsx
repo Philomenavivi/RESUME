2ec8196e04f5bb450d5f8da2a71f3cb79097c25a
phillomena vivi <philomenanzisa22@gmail.com>
2026-06-19 13:03:34 +0300

    your message here

--- portfolio/app/component/page.tsx
+++ portfolio/app/component/page.tsx
@@ -53,17 +53,19 @@ export default function Component() {
       name: "Admin Dashboard",
       tech: ["Next.js"],
       status: "In Progress",
-    }
-
+    },
   ];
 
   return (
-    <main className="min-h-screen bg-gray-400 flex justify-center py-10 px-4">
-      <div className="w-full max-w-xl bg-gray-300 shadow-xl flex">
+    <main className="min-h-screen bg-gray-400 flex items-center justify-center py-10 px-4">
+      
+      
+      <div className="w-full max-w-6xl bg-gray-300 shadow-xl flex flex-col md:flex-row">
 
+  
+        <aside className="w-full md:w-2/5 p-4 shadow-lg bg-gray-600">
 
-        <aside className="w-1/3 p-4 shadow-lg bg-gray-600">
-          <div className="relative w-20 h-20 item-center mx-auto rounded-full overflow-hidden border-4 border-white">
+          <div className="relative w-60 h-60 mx-auto rounded-full overflow-hidden border-4 border-white">
             <Image
               src="/image/red.jpeg"
               alt="redimage"
@@ -71,79 +73,85 @@ export default function Component() {
               className="object-cover"
             />
           </div>
-          <div className="hidden:block font-sm  justify-between p-2 bg-gray-400  ">
 
-            <h2 className="mt-3 font-sm text-sm font-bold">CONTACT</h2>
-            <p className="text-sm font-sm item-left">📞0745200020</p>
-            <p className="text-sm font-sm item-left gap-0">email</p>
-            <p className="text-sm item-left">📍 Nairobi, Kenya</p>
+          <div className="block font-sm justify-between p-2 bg-gray-400 mt-4">
+            <h2 className="mt-3 text-sm font-bold">CONTACT</h2>
+            <p className="text-xl">📞0745200020</p>
+            <p className="text-xl">email</p>
+            <p className="text-xl">📍 Nairobi, Kenya</p>
           </div>
 
-          <h2 className="mt-5 font-bold">SKILLS</h2>
-          <ul className="text-sm text-left space-y-1">
-            <li> Computer Hardware & Software Troubleshooting</li>
-            <li> Network Setup & Basic Configuration (LAN/WAN)</li>
-            <li> Microsoft Office 365 (Word, Excel, PowerPoint, Outlook)</li>
-            <li> Database Basics (MySQL, SQL Queries)</li>
-            <li> Web Development (HTML, CSS, JavaScript ,nextjs ,react)</li>
+          <h2 className="mt-5 text-2xl font-bold">SKILLS</h2>
+
+          <ul className="text-sm text-left space-y-2">
+            <li>Computer Hardware & Software Troubleshooting</li>
+            <li>Network Setup & Basic Configuration (LAN/WAN)</li>
+            <li>Microsoft Office 365 (Word, Excel, PowerPoint, Outlook)</li>
+            <li>Database Basics (MySQL, SQL Queries)</li>
+            <li>Web Development (HTML, CSS, JavaScript ,nextjs ,react)</li>
             <li>IT Support & Help Desk Assistance</li>
-            <li> Cybersecurity Awareness & Basic Protection Practices</li>
-            <li> System Installation & Maintenance </li>
-            <li> File Management, Backup & Recovery </li>
+            <li>Cybersecurity Awareness & Basic Protection Practices</li>
+            <li>System Installation & Maintenance</li>
+            <li>File Management, Backup & Recovery</li>
             <li>Cloud Basics (Google Drive, OneDrive, Introduction to AWS)</li>
-            <li> Networking Fundamentals (IP Addressing, DNS, DHCP)</li>
-            <li> Basic Programming Concepts</li>
-            <li> Technical Documentation & Reporting </li>
-            <li>roblem Solving & Customer Support Skills</li>
+            <li>Networking Fundamentals (IP Addressing, DNS, DHCP)</li>
+            <li>Basic Programming Concepts</li>
+            <li>Technical Documentation & Reporting</li>
+            <li>problem Solving & Customer Support Skills</li>
             <li>Teamwork & Communication in IT Environment</li>
-            <li> Hardware Setup (Printers, Routers, PCs)</li>
+            <li>Hardware Setup (Printers, Routers, PCs)</li>
             <li>nextjs, react</li>
           </ul>
 
         </aside>
 
+      
+        <section className="w-full md:w-3/5 bg-gray-400 p-8">
 
-        <section className="w-2/3 p-8">
+          <h1 className="text-3xl text-black font-bold">Philomena Vivi</h1>
+          <p className="font-bold text-2xl">Front-End Developer</p>
 
-          <h1 className="text-3xl text-sm font-bold">Philomena Vivi</h1>
-          <p className="text-gray-600">Front-End Developer</p>
+          <h2 className="mt-5 font-bold text-2xl">Training</h2>
 
-
-          <h2 className="mt-5 font-bold text-xl">Training</h2>
           {training.map((job, index) => (
             <div key={index} className="mt-2">
-              <p className="font-semibold">{job.years}</p>
+              <p className="font-bold text-2xl">{job.years}</p>
               <p>{job.title}</p>
-              <p className="text-gray-600">{job.company}</p>
+              <p className="text-2xl font-bold">{job.company}</p>
             </div>
           ))}
 
+          <h2 className="mt-6 bg-gray-500 font-bold text-3xl">
+            Education
+          </h2>
 
-          <h2 className="mt-6 font-bold text-xl">Education</h2>
           {education.map((item, index) => (
-            <div key={index} className="mt-3">
+            <div key={index} className="mt-4">
               <p className="font-semibold">{item.year}</p>
               <p>{item.education}</p>
-              <p className="text-black-600 text-sm">{item.school}</p>
+              <p className="text-2xl">{item.school}</p>
             </div>
           ))}
 
+          <h2 className="mt-6 font-bold text-3xl">Projects</h2>
 
-          <h2 className="mt-6 font-bold text-xl">Projects</h2>
-
-          <div className="mt-3 space-y-3">
+          <div className="mt-4 space-y-4">
             {project.map((proj, index) => (
-              <div key={index} className="p-3 bg-gray text-black rounded shadow">
-                <p className="font-semibold">{proj.name}</p>
-                <p className="text-sm text-gray-600">
+              <div
+                key={index}
+                className="p-3 bg-gray-500 text-black rounded shadow"
+              >
+                <p className="font-bold">{proj.name}</p>
+                <p className="text-xl">
                   Tech: {proj.tech.join(", ")}
                 </p>
-                <p className="text-sm">{proj.status}</p>
+                <p className="text-xl">{proj.status}</p>
               </div>
             ))}
           </div>
 
         </section>
+
       </div>
     </main>
   );
--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col-10 gap-3">
-        <div className="col-span-3">
+        <div className="col-span-4 justify-between">
           <Component />
         </div>
         
